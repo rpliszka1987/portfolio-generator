7b58cffeb8dd031c870b76d483c07a1e8d264b23
Robert Pliszka <robplisz@gmail.com>
2022-04-27 11:15:52 -0400

    Finished the html displaying using promises

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
+const {writeFile, copyFile} = require('./utils/generate-site.js');
 
 
 const promptUser = () => {
@@ -135,19 +136,18 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('./dist/index.html', pageHTML, err => {
-            if (err) throw err;
-
-            console.log('Portfolio complete! Checkout index.html to see the output!');
-
-            fs.copyFile('./src/style.css', './dist/style.css', err => {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-                console.log('Style sheet copies successfully!');
-            });
-        });
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
